d81bbeb198922c4626db60b09a6d68d20e678def
김동현 <kimdh@pcninc.co.kr>
2021-08-10 16:00:20 +0900

    초기 소스 커밋

---
+++ srj/src/main/java/com/saerom/srj/SrjApplication.java
@@ -0,0 +1,13 @@
+package com.saerom.srj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrjApplication.class, args);
+	}
+
+}
---
+++ srj/src/test/java/com/saerom/srj/SrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saerom.srj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
